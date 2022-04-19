7fe80e9b07c231299bcfeb291e349cba2e63d53d
Alexander <mobotsar@protonmail.com>
2022-04-19 00:20:32 -0400

    I can now parse lambda expressions with an ll(1) recursive descent function.

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,18 @@
 open Stratagem
 open Util
 
-let ts = Front.lex "#barney.#c.barney (c) (#t.#f.f)"
+let ts = Front.lex "#g.(((#barney_the. #    c.purple_dinosaur c) #t.#f.f) there_we_go_ish)"
 
 let () = print_endline @@ Ls.sol Front.tts ts
 
 let () = print_endline "It's not broken yet!"
+
+let tree = Front.parse_prog @@ ref ts
+
+let rec p (t : Front.term) : unit =
+  match t with
+  | Name (_, n) -> print_string n
+  | App (_, t1, t2) -> print_string "(" ; p t1 ; print_string " " ; p t2 ; print_string ")"
+  | Abstr (_, n, t) -> print_string "λ" ; p n ; print_char '.' ; p t
+
+let _ = p tree ; print_endline ""
--- lib/front.mli
+++ lib/front.mli
@@ -1,3 +1,7 @@
-type token = Name of string | Dot | Lambda | OpenParen | CloseParen | Space
+type token = NameTok of string | Dot | Lambda | OpenParen | CloseParen | Space | End
+type info = { loc : int * int }
+type term = Name of info * string | Abstr of info * term * term | App of info * term * term
+val parse_prog : token list ref -> term
 val tts : token -> string
 val lex : string -> token list
+val lambda_char : char
