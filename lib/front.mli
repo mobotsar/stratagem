ee592b301ea1d19a2c4d247fce695f3d561c606d
Alexander Lucas <mobotsar@pm.me>
2022-08-13 23:35:35 -0400

    initial haskellification

--- lib/front.mli
+++
@@ -1,7 +0,0 @@
-type token = NameTok of string | Dot | Lambda | OpenParen | CloseParen | Space | End
-type info = { loc : int * int }
-type term = Name of info * string | Abstr of info * term * term | App of info * term * term
-val parse_prog : token list -> term
-val tts : token -> string
-val lex : string -> token list
-(* val lambda_char : cha *)
